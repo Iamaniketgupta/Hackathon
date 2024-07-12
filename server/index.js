b0a8e1e836d735a48d3533a15889650576ffc5ca
Aniket Gupta <aniketgupta.org@gmail.com>
2024-07-12 15:06:33 +0530

    added env

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
 import 'dotenv/config';
-import cors from 'cors';
 import app from './src/app.js';
 import dbConnect from './config/db.js';
 
-app.use(cors({
-    origin : process.env.CLIENT_URL || '*',
-}));
-
-app.use(express.json());
 
 dbConnect().then(()=>
     app.listen(process.env.PORT||8000,()=>{
